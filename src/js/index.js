f37f2692cd48553ea2c40a36b0e3db14e4df1374
Gustav Larsson <gl222pp@student.lnu.se>
2024-09-13 08:17:52 +0200

    Add js and css files

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Gustav Larsson <gl222pp@student.lnu.se>
+ * @version 1.0.0
+ */
+
+const helloWorld = document.querySelector('#app')
+const helloWorldText = document.createTextNode('Hello World!')
+helloWorld.appendChild(helloWorldText)
